1288c31e641e507b64e8a587a1f2e00186449b07
rafaelwnk <rafaelcarmo749@gmail.com>
2026-08-22 12:36:13 -0300

    chore: add initial migration

---
+++ backend/src/Inventory/Inventory.Infrastructure/Migrations/20260822153339_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Code = table.Column<string>(type: "VARCHAR", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "VARCHAR", maxLength: 40, nullable: false),
+                    StockBalance = table.Column<int>(type: "INT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMPTZ", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMPTZ", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Product_Code",
+                table: "Products",
+                column: "Code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
