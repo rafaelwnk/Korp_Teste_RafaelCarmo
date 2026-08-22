1b14bc4c55feb4e8eb56ed14a411bab24d74e5cd
rafaelwnk <rafaelcarmo749@gmail.com>
2026-08-22 12:47:57 -0300

    feat: add endpoints, extensions and configure builder/app

--- backend/src/Inventory/Inventory.Api/Program.cs
+++ backend/src/Inventory/Inventory.Api/Program.cs
@@ -1,7 +1,20 @@
+using Inventory.Api.Endpoints;
+using Inventory.Api.Extensions;
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServices();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
 app.UseHttpsRedirection();
+app.MapProductsEndpoints();
 
 app.Run();
