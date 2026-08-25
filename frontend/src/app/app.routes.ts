e748d114fd8a3d348de329983345a9e2fc97d4a0
rafaelwnk <rafaelcarmo749@gmail.com>
2026-08-25 18:41:05 -0300

    feat(frontend): add home page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainLayout } from './layout/main-layout/main-layout';
-import { Products } from './pages/products/products';
 
 export const routes: Routes = [
     {
         path: '',
         component: MainLayout,
         children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/home/home').then(m => m.Home)
+            },
             {
                 path: 'products',
                 loadComponent: () => import('./pages/products/products').then(m => m.Products)
